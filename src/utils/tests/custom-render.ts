44c201b422759e2ed4de3119c4053c6ceb6a8846
Junior Alves <jjalves96@gmail.com>
2023-05-28 07:53:42 -0300

    feat: create customRender utils

---
+++ src/utils/tests/custom-render.ts
@@ -0,0 +1,11 @@
+import { ReactElement } from 'react';
+
+import { render, RenderOptions } from '@testing-library/react';
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>
+) => render(ui, { ...options });
+
+export * from '@testing-library/react';
+export { customRender as render };
