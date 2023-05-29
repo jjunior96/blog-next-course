53563d8a27c9c1570734ec495d2293d78816870a
Junior Alves <jjalves96@gmail.com>
2023-05-29 08:24:09 -0300

    chore: add first post example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import { allPosts } from 'contentlayer/generated';
+import Image from 'next/image';
+
 export default function Home() {
+  const posts = allPosts.map((post) => post);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h2>Hellor world</h2>
+
+      <div dangerouslySetInnerHTML={{ __html: posts[0]?.title }} />
+
+      <Image src={posts[0].image} width={700} height={700} alt="" />
     </main>
   );
 }
