28bfa3f508516d81e759394f13791ac2c38e1676
Junior Alves <jjalves96@gmail.com>
2023-06-01 06:10:16 -0300

    feat: create configs

---
+++ src/config/header.ts
@@ -0,0 +1,18 @@
+import { HeaderNav } from '@/types';
+
+export const headerConfig: HeaderNav = {
+  mainNav: [
+    {
+      title: 'Home',
+      href: '/'
+    },
+    {
+      title: 'Artigos',
+      href: '/articles'
+    },
+    {
+      title: 'Sobre mim',
+      href: '/about'
+    }
+  ]
+};
---
+++ src/config/site.ts
@@ -0,0 +1,15 @@
+import { Site } from '@/types';
+
+export const siteConfig: Site = {
+  name: 'Junior Alves',
+  description: 'Blog onde falo sobre código e livros',
+  title: 'Senior Software Engineer e content creator.',
+  subtitle:
+    'Bem vindo ao meu blog! Eu sou Junior Alves, apaixonado por tecnologia, sempre buscando aprender algo novo todos os dias.',
+  url: 'https://meusite.com.br',
+  links: {
+    youtube: 'https://www.youtube.com/@devjunioralves',
+    github: 'https://github.com/jjunior96',
+    linkedin: 'https://www.linkedin.com/in/junior-alves-b66a10127/'
+  }
+};
