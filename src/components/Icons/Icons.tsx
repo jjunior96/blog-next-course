982cf9ea8363d08506fa21c5f856266d7b1e3dd1
Junior Alves <jjalves96@gmail.com>
2023-06-07 08:37:39 -0300

    chore: add new icons

--- src/components/Icons/Icons.tsx
+++ src/components/Icons/Icons.tsx
@@ -3,5 +3,13 @@
 export {
   // General
   X as CloseIcon,
-  List as OpenIcon
+  List as OpenIcon,
+
+  // Social Icons
+  FacebookLogo as FacebookIcon,
+  LinkedinLogo as LinkedinIcon,
+  YoutubeLogo as YoutubeIcon,
+  InstagramLogo as InstagramIcon,
+  TwitterLogo as TwitterIcon,
+  GithubLogo as GithubIcon
 } from '@phosphor-icons/react';
