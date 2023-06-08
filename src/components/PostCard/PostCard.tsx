0ad706b274a971ceef91ec489e60cc29c14ad359
Junior Alves <jjalves96@gmail.com>
2023-06-08 16:27:52 -0300

    feat: create PostCard styles

--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -1,43 +1,42 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import { formatDate } from '@/functions';
 
 import { Tag } from '@/components/Tag';
 
-export const PostCard = () => {
+import { BlogPost } from '@/models';
+
+import * as S from './styles';
+
+type PostCardProps = {
+  post: BlogPost;
+};
+
+export const PostCard = ({ post }: PostCardProps) => {
+  const { frontmatter, readingTime, slug } = post;
+  const { title, description, date, image, tags } = frontmatter;
+
+  const formattedDate = formatDate(date);
+
   return (
-    <>
-      <Link href="#">
-        <div className=" relative h-80 w-full">
-          <Image
-            src="/assets/images/image-post.jpeg"
-            fill
-            alt="title"
-            priority
-            className="rounded-xl object-cover object-center"
-          />
-        </div>
+    <S.LinkContainer href={slug}>
+      <S.ImageContainer>
+        <S.Image src={image} fill alt="title" priority />
+      </S.ImageContainer>
 
-        <div className="pt-3">
-          <div className="mb-3 flex flex-wrap gap-2">
-            {['TS', 'JS', 'React'].map((tag) => (
-              <Tag key={tag}>{tag}</Tag>
-            ))}
-          </div>
+      <S.Content>
+        <S.TagsContainer>
+          {tags?.map((tag) => (
+            <Tag key={tag}>{tag}</Tag>
+          ))}
+        </S.TagsContainer>
 
-          <time className="text-gray-400">
-            15 de maio de 2023 • 3 minutos de leitura
-          </time>
+        <S.Time>
+          {formattedDate} • {readingTime} minutos de leitura
+        </S.Time>
 
-          <p className="mt-2 max-w-md text-ellipsis text-2xl font-medium text-gray-50">
-            O que é Dependency Injection e como aplicar no React.js?
-          </p>
+        <S.Title>{title}</S.Title>
 
-          <p className="mt-3 text-gray-400">
-            Vamos aprender à como aplicar esse conceito tão importante no
-            React.js!
-          </p>
-        </div>
-      </Link>
-    </>
+        <S.Description>{description}</S.Description>
+      </S.Content>
+    </S.LinkContainer>
   );
 };
