369fab20040a9cf3c861c64e2caa5e484bc12a69
Junior Alves <jjalves96@gmail.com>
2023-06-14 07:28:05 -0300

    chore: add general improviments

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import { notFound } from 'next/navigation';
+
 import { PostService } from '@/services';
 
 import { Post } from '@/components/Post';
@@ -5,9 +7,8 @@ import { Post } from '@/components/Post';
 export default function PostPage({ params }: { params: { slug: string } }) {
   const post = PostService.getBySlug(params.slug);
 
-  // TODO: checar se o post existe
   if (!post) {
-    return null;
+    notFound();
   }
 
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ import { Profile } from '@/components/Profile';
 
 export default function Home() {
   const { posts, currentPage, numbPages } = PostService.getAll();
-
   const { prevPage, nextPage } = paginationPages(currentPage);
 
   return (
